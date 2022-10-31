3c1c3cc269ba8bcb3999e43478fe1fac54c6a555
Mytran0505 <mytran0505itlker@gmail.com>
2022-10-31 19:38:46 +0700

    Connect Wallets ( UPDATE )

--- didem/tutorials/bored-nft-minting-dapp/dapp.config.js
+++ didem/tutorials/bored-nft-minting-dapp/dapp.config.js
@@ -1,7 +1,4 @@
 
-const FORTMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY;
-const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL;
-
 const config ={
     title:'BoredApe Dapp',
     description:'A showcase dapp which is buitl for you to create your own dapp',
@@ -11,54 +8,54 @@ const config ={
     price:0.01
 }
 
-const onboardOPtions ={
-    dappId: process.env.NEXT_PUBLIC_DAPP_ID,
-    networkId: 4,
-    darkMode: true,
-    walletSelect:{
+// const onboardOPtions ={
+//     dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+//     networkId: 4,
+//     darkMode: true,
+//     walletSelect:{
        
-        wallets:[
-            { walletName: "metamask", preferred: true },
-            { walletName: "coinbase", preferred: true },
-            {
-                walletName: 'walletLink',
-                preferred: true,
-                rpcUrl: RPC_URL,
-                appName:'BoredApe Dapp',
-            },
-            {
-                walletName: "fortmatic",
-                apiKey: FORTMATIC_KEY,
-                preferred: true
-            },
-            { walletName: "trust", preferred: true, rpcUrl: RPC_URL },
-            {
-                walletName: 'gnosis',
-                preferred: true
-            },
-            { walletName: "authereum" },
+//         wallets:[
+//             { walletName: "metamask", preferred: true },
+//             { walletName: "coinbase", preferred: true },
+//             {
+//                 walletName: 'walletLink',
+//                 preferred: true,
+//                 rpcUrl: RPC_URL,
+//                 appName:'BoredApe Dapp',
+//             },
+//             {
+//                 walletName: "fortmatic",
+//                 apiKey: FORTMATIC_KEY,
+//                 preferred: true
+//             },
+//             { walletName: "trust", preferred: true, rpcUrl: RPC_URL },
+//             {
+//                 walletName: 'gnosis',
+//                 preferred: true
+//             },
+//             { walletName: "authereum" },
             
-            {
-                walletName: 'ledger',
-                rpcUrl: RPC_URL
-            },
-            {
-                walletName: 'lattice',
-                rpcUrl: RPC_URL,
-                appName: "BoredApe Dapp"
-            },
-            {
-                walletName: 'keepkey',
-                rpcUrl: RPC_URL
-            }
-        ]
-    },
-    walletCheck:[
-        { checkName: 'derivationPath' },
-        { checkName: 'accounts' },
-        { checkName: 'connect' },
-        { checkName: 'network' },
-    ]
-}
+//             {
+//                 walletName: 'ledger',
+//                 rpcUrl: RPC_URL
+//             },
+//             {
+//                 walletName: 'lattice',
+//                 rpcUrl: RPC_URL,
+//                 appName: "BoredApe Dapp"
+//             },
+//             {
+//                 walletName: 'keepkey',
+//                 rpcUrl: RPC_URL
+//             }
+//         ]
+//     },
+//     walletCheck:[
+//         { checkName: 'derivationPath' },
+//         { checkName: 'accounts' },
+//         { checkName: 'connect' },
+//         { checkName: 'network' },
+//     ]
+// }
 
-export{config, onboardOPtions}
+export{config}
